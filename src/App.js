31388c4f589dcbf73cd11a2293028f98505e1f05
PabloPicas0 <pawel.fe@gmail.com>
2023-01-10 23:34:05 +0100

    basic app concept

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './index.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate, faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import "./index.scss";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="app-container">
+        <img />
+        <div className="basic-info">
+          <p>
+            25*c
+            <button className="refresh">
+              <FontAwesomeIcon icon={faArrowsRotate} />
+            </button>
+          </p>
+          <p>Location</p>
+        </div>
+
+        <div className="more-info">
+          <button>
+              <FontAwesomeIcon icon={faCaretDown} />
+            More Info...
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
