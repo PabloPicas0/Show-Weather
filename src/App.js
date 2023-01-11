d29d7170db74f3ee7248cd150407c33f35f2fa18
PabloPicas0 <pawel.fe@gmail.com>
2023-01-11 23:24:10 +0100

    added state for fetched data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useEffect, useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowsRotate, faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
 import "./index.scss";
 
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState(null);
+
   return (
     <div className="App">
       <div className="app-container">
         <img />
         <div className="basic-info">
-          <button className="refresh">
+          <button type="button" className="refresh">
             <FontAwesomeIcon icon={faArrowsRotate} />
           </button>
           <p>25*c</p>
@@ -18,14 +23,14 @@ function App() {
 
         <div className="more-info">
           <div>
-            <button>
+            <button type="button">
               <FontAwesomeIcon icon={faCaretDown} />
               More Info...
             </button>
           </div>
         </div>
       </div>
-      <footer>FCC free weather app</footer>
+      <footer>FCC Free Weather App</footer>
     </div>
   );
 }
