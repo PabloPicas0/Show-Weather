32f750d487d03e5b88bb07eedf748592295fe2de
PabloPicas0 <pawel.fe@gmail.com>
2023-07-13 14:42:57 +0200

    deleted api response in console

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ function App() {
 
   useEffect(() => {
     fetchWeatherInfo(latitude, longitude).then((weather) => {
-      console.log(weather);
       setWeatherInfo(weather);
     });
   }, []);
